7fb91ec348e537580bec07c1314ba50693dc9d30
Luis Palacios <luisarmando20092009@gmail.com>
2025-05-28 09:10:47 -0400

    [fix]error

--- backend/config.py
+++ backend/config.py
@@ -1,3 +1,7 @@
+import os
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/flask_crud' # DB URI
-    SQLALCHEMY_TRACK_MODIFICATIONS = False # Desactivar la modificaciones de SQLAlchemy
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'SQLALCHEMY_DATABASE_URI',
+        'postgresql://postgres:postgres@localhost:5432/flask_crud'
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
